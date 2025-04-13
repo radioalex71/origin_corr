e8b666bf8e99c6032089f38b797e48d4cb1f0848
Alexey Ryabinkin <a.ryabinkin71@gmail.com>
2025-04-13 18:59:39 +0300

    Task_3_7_02

---
+++ lesson_7/task_7_02/Macros_2.cpp
@@ -0,0 +1,23 @@
+﻿// NetoHomework_3_7_02
+// Author: Ryabinkin Alexey
+// Data: 13.04.25
+
+#include <iostream>
+#define SUB(a, b) ((a)-(b)) //макрос разности 2-х чисел
+
+
+int main(int argc, char** argv)
+{
+    //инициализируем переменные
+    int a = 20;
+    int b = 10;
+    int c = 3;
+    
+    //вызывваем макрос SUB для выполнения 3-х задач
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << SUB(a, b) * c << std::endl;
+    std::cout << SUB(a, b + c) * c << std::endl;
+
+    return 0;
+}
+
